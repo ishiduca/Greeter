ce833d6e7e8098d2af9576afdf961b81935b6ecc
ishiduca <ishiduca@gmail.com>
2014-01-04 20:59:22 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Greeter - It's new $module
+
+# SYNOPSIS
+
+    use Greeter;
+
+# DESCRIPTION
+
+Greeter is ...
+
+# LICENSE
+
+Copyright (C) ishiduca.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ishiduca <ishiduca@gmail.com>
---
+++ lib/Greeter.pm
@@ -0,0 +1,39 @@
+package Greeter;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Greeter - It's new $module
+
+=head1 SYNOPSIS
+
+    use Greeter;
+
+=head1 DESCRIPTION
+
+Greeter is ...
+
+=head1 LICENSE
+
+Copyright (C) ishiduca.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ishiduca E<lt>ishiduca@gmail.comE<gt>
+
+=cut
+
